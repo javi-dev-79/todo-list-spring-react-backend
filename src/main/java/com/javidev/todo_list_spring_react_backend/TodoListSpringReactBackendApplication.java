e16a3aa90caba9ce9d01df74169c0fd4b11fad63
javi-dev-79 <javi.dev.79@gmail.com>
2025-03-10 10:29:40 +0000

    First commit

---
+++ src/main/java/com/javidev/todo_list_spring_react_backend/TodoListSpringReactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.javidev.todo_list_spring_react_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListSpringReactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListSpringReactBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javidev/todo_list_spring_react_backend/TodoListSpringReactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javidev.todo_list_spring_react_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListSpringReactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
